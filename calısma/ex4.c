d682aa5d29f4890ae1a21886437327baab242986
Ali Erdem <alierdemgdk58@gmail.com>
2025-10-21 01:15:44 +0300

    auto(save): 2025-10-21 01:15:43

---
+++ calısma/ex4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+    int age;
+    printf("yasinizi girin : ");
+    scanf("%d",age);
+
+    if (age >= 18)
+    {
+        printf("resitsiniz\n")
+    
+    }else
+    
+}
