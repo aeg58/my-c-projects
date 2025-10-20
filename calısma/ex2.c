c12fc851a1dc2346f498be8616cbc460d14c8bd8
Ali Erdem <alierdemgdk58@gmail.com>
2025-10-21 00:10:10 +0300

    init: first commit

---
+++ calısma/ex2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+    int age;
+    float height;
+    char initial;
+
+    printf("enter your age");
+    scanf("%d",&age);
+
+    printf("enter your height");
+    scanf("%f",&height);
+
+    printf("Enter your initial: ");
+    scanf(" %c", &initial);
+
+    printf("\n You entered : \n");
+    printf("Age : %d\n",age);
+    printf("height : %.2f m \n",height);
+    printf("initial : %c \n ",initial);
+
+    
+    return 0;
+
+}
