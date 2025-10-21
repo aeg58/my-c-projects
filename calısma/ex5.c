793abd3f60f60271fa8d8aa31f25794615af31fd
Ali Erdem <alierdemgdk58@gmail.com>
2025-10-21 09:55:19 +0300

    auto(save): 2025-10-21 09:55:19

--- calısma/ex5.c
+++ calısma/ex5.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
-int main(void) {
-   int sayilar[5];
-   int i,toplam = 0;
-   float ort;
+int main(void)
+{
+    int sayilar[5];
+    int i, toplam = 0;
+    float ort;
 
-   printf("5 sayi girin : \n");
-   for ( i = 0; i < count; i++)
-   {
-    scanf
-   }
-   
+    printf("5 sayi girin : \n");
+    for (i = 0; i < count; i++)
+    {
+        scanf
+    }
 }
