ff06615994a0a0d625d5f80e296622d0ae4f9936
Ali Erdem <alierdemgdk58@gmail.com>
2025-10-21 09:27:45 +0300

    auto(save): 2025-10-21 09:27:45

--- calısma/ex5.c
+++ calısma/ex5.c
@@ -1,33 +1,5 @@
 #include <stdio.h>
 
 int main(void) {
-    int secim;
-    do {
-        printf("\n=== Menü ===\n");
-        printf("1. Merhaba yaz\n");
-        printf("2. Sayıları topla\n");
-        printf("3. Çıkış\n");
-        printf("Seçiminiz: ");
-        scanf("%d", &secim);
-
-        switch (secim) {
-            case 1:
-                printf("Merhaba, C öğreniyorsun!\n");
-                break;
-            case 2: {
-                int a, b;
-                printf("İki sayı gir: ");
-                scanf("%d %d", &a, &b);
-                printf("Toplam: %d\n", a + b);
-                break;
-            }
-            case 3:
-                printf("Program sonlandırıldı.\n");
-                break;
-            default:
-                printf("Geçersiz seçim!\n");
-        }
-    } while (secim != 3);
-
-    return 0;
+    
 }
