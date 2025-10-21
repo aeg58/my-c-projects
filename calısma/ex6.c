42e730a1c7a2c3a5a49aa059287c4447abd9c9a8
Ali Erdem <alierdemgdk58@gmail.com>
2025-10-21 12:00:17 +0300

    auto(save): 2025-10-21 12:00:17

--- calısma/ex6.c
+++ calısma/ex6.c
@@ -1,32 +1,3 @@
 #include <stdio.h>
 
-int main(void)
-{
-    int dizi[5];
-    int i, j, temp;
-    for (i = 0; i < 5; i++)
-    {
-        printf("%d. sayiyi giriniz: ", i + 1);
-        scanf("%d", &dizi[i]);
-    }
-
-    for (i = 0; i < 5 - 1; i++)
-    {
-        for (j = 0; j < 5 - i - 1; j++)
-        {
-            if (dizi[j] < dizi[j + 1])
-            {
-                temp = dizi[j];
-                dizi[j] = dizi[j + 1];
-                dizi[j + 1] = temp;
-            }
-        }
-    }
-
-    printf("Siralanmis dizi: ");
-    for (i = 0; i < 5; i++)
-    {
-        printf("%d ", dizi[i]);
-    }
-    return 0;
-}
+int main()
