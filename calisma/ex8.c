812a4d0558c145dcd802491afb999fa96c101663
Ali Erdem <alierdemgdk58@gmail.com>
2025-10-21 13:37:34 +0300

    auto(save): 2025-10-21 13:37:34

--- calisma/ex8.c
+++ calisma/ex8.c
@@ -3,12 +3,9 @@
 int main(void)
 {
     int sayilar[5] = {10, 20, 30, 40, 50};
-    int *p = sayilar; 
+    int *p = a; 
 
-    printf("sayilar[0] = %d\n", sayilar[0]);
-    printf("*p = %d\n", *p);
-    printf("*(p+1) = %d\n", *(p + 1));
-    printf("*(p+2) = %d\n", *(p + 2));
+   
 
 
 
