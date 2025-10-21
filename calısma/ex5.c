44cdd73ba587c77826a9e874d5d08d87bde1192f
Ali Erdem <alierdemgdk58@gmail.com>
2025-10-21 10:05:39 +0300

    auto(save): 2025-10-21 10:05:38

--- calısma/ex5.c
+++ calısma/ex5.c
@@ -3,26 +3,7 @@
 int main(void)
 {
     int sayilar[5];
-    int i, min, max;
+    int i, 
 
-    printf("5 sayi girin : \n");
-    for (i = 0; i < 5; i++)
-    {
-        scanf("%d", &sayilar[i]);
-        
-    }
-
-    min = sayilar[0];
-    max = sayilar[0];
-
-    for (i = 1; i < 5; i++)
-    {
-        if (sayilar[i] < min)
-            min = sayilar[i];
-        if (sayilar[i] > max)
-            max = sayilar[i];
-    }
-
-    printf("En küçük: %d\n", min);
-    printf("En büyük: %d\n", max);
+    
 }
