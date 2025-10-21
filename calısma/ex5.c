facd66caee24af33e9de2900d2d48ef8f4d6469a
Ali Erdem <alierdemgdk58@gmail.com>
2025-10-21 10:01:31 +0300

    auto(save): 2025-10-21 10:01:31

--- calısma/ex5.c
+++ calısma/ex5.c
@@ -15,12 +15,14 @@ int main(void)
     min = sayilar[0];
     max = sayilar[0];
 
-    for (i = 1; i < 5; i++) {
+    for (i = 1; i < 5; i++)
+    {
         if (sayilar[i] < min)
             min = sayilar[i];
-        if (sayilar[i]>max)
-            max = sayilar[i];           
+        if (sayilar[i] > max)
+            max = sayilar[i];
     }
 
-    
+    printf("En küçük: %d\n", min);
+    printf("En büyük: %d\n", max);
 }
