093c1f83b8ea6488d6e9103c3fb7e0d4d98eed44
Ali Erdem <alierdemgdk58@gmail.com>
2025-10-21 13:11:32 +0300

    auto(save): 2025-10-21 13:11:32

--- calisma/ex8.c
+++ calisma/ex8.c
@@ -8,8 +8,9 @@ int main(void)
     printf("sayilar[0] = %d\n", sayilar[0]);
     printf("*p = %d\n", *p);
     printf("*(p+1) = %d\n", *(p + 1));
+    printf("*(p+2) = %d\n", *(p + 2));
 
-    
+
 
     return 0;
 }
