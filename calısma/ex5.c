7c0bcfb38b8b186a566bc778a6ea8d7f1824e75b
Ali Erdem <alierdemgdk58@gmail.com>
2025-10-21 09:54:38 +0300

    auto(save): 2025-10-21 09:54:37

--- calısma/ex5.c
+++ calısma/ex5.c
@@ -1,15 +1,5 @@
 #include <stdio.h>
 
 int main(void) {
-    int dizi[3];
-
-    dizi[0] = 10;
-    dizi[1] = 20;
-    dizi[2] = 30;
-
-    printf("0. eleman: %d\n", dizi[0]);
-    printf("1. eleman: %d\n", dizi[1]);
-    printf("2. eleman: %d\n", dizi[2]);
-
-    return 0;
+   
 }
