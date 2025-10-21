ea4a7aef282e8dcb7bccb64a1bb556eda91867d6
Ali Erdem <alierdemgdk58@gmail.com>
2025-10-21 10:08:59 +0300

    auto(save): 2025-10-21 10:08:59

--- calısma/ex5.c
+++ calısma/ex5.c
@@ -4,17 +4,16 @@ int main(void)
 {
     int sayilar[5];
     int i, temp;
-    for(i = 0; i < 5; i++)
+    for (i = 0; i < 5; i++)
     {
         printf("%d. sayiyi giriniz: ", i + 1);
         scanf("%d", &sayilar[i]);
     }
 
-    for ( i = 0; i < 5-i; i++){
-    for (size_t i = 0; i < count; i++){
-
+    for (i = 0; i < 5 - i; i++)
+    {
+        for (size_t i = 0; i < count; i++)
+        {
+        }
     }
-    
-    
-       }
 }
