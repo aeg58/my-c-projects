18e5574a25a25ff32652aaf3f3f6385c8538dfbb
Ali Erdem <alierdemgdk58@gmail.com>
2025-10-21 19:48:04 +0300

    auto(save): 2025-10-21 19:48:04

--- calisma/gpt_ile_calisma2/x1.c
+++ calisma/gpt_ile_calisma2/x1.c
@@ -1,12 +1,5 @@
 #include <stdio.h>
 
 int main(void) {
-    char mesaj[] = "Merhaba";
-    char *p = &mesaj[0];
-
-    printf("p adresi      = %p\n", (void*)p);
-    printf("p + 1 adresi  = %p\n", (void*)(p + 1));
-    printf("p + 2 adresi  = %p\n", (void*)(p + 2));
-
-    return 0;
+    char
 }
