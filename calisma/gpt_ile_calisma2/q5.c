66a927b78b9471b095a59151e7769bb126263cad
Ali Erdem <alierdemgdk58@gmail.com>
2025-11-07 00:22:28 +0300

    auto(save): 2025-11-07 00:22:28

--- calisma/gpt_ile_calisma2/q5.c
+++ calisma/gpt_ile_calisma2/q5.c
@@ -8,10 +8,6 @@ int main(){
     for ( i = 0; i < 5; i++)
     {
         temp = arr[i]
-        for (size_t i = 0; i < count; i++)
-        {
-            /* code */
-        }
         
     }
     
