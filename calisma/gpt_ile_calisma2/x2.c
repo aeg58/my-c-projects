cb8e35519c583efca98067c76b92338cbfc9e7c3
Ali Erdem <alierdemgdk58@gmail.com>
2025-11-07 00:06:43 +0300

    auto(save): 2025-11-07 00:06:43

--- calisma/gpt_ile_calisma2/x2.c
+++ calisma/gpt_ile_calisma2/x2.c
@@ -10,9 +10,10 @@ int main()
         scanf("%d", &arr[i]);
     }
     printf("------------------------------\n");
-    
+
     printf("arr     = %p\n", arr);
     printf("&arr[0] = %p\n", &arr[0]);
     printf("&arr    = %p\n", &arr);
+    printf
     return 0;
 }
