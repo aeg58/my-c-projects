18a19dcd97163d6bd0c7eaa3b76038b52721fd0b
Ali Erdem <alierdemgdk58@gmail.com>
2025-11-07 00:11:10 +0300

    auto(save): 2025-11-07 00:11:10

--- calisma/gpt_ile_calisma2/x3.c
+++ calisma/gpt_ile_calisma2/x3.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 
-int main(){
-    int arr[5];
-    arr = {1,2,3,4,5};
+int main()
+{
+    int arr[5] = {1, 2, 3, 4, 5};
+
     int *ptr;
     ptr = arr;
 
     for (int i = 0; i < 5; i++)
     {
-        printf("%d\n",*(ptr + i));
+        printf("%d\n", *(ptr + i));
     }
-    
-
 }
