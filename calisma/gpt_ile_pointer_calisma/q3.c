fc9e85772f80ff692ec37606ad10bfb31c14eb29
Ali Erdem <alierdemgdk58@gmail.com>
2025-11-08 15:14:25 +0300

    auto(save): 2025-11-08 15:14:25

--- calisma/gpt_ile_pointer_calisma/q3.c
+++ calisma/gpt_ile_pointer_calisma/q3.c
@@ -6,5 +6,10 @@ int main()
         {4, 2, 9},
         {5, 7, 1}};
 
-        fpr
+          for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("Address of arr[%d][%d] = %p  |  Value = %d\n",
+                   i, j, (*(arr + i) + j), *(*(arr + i) + j));
+        }
+    }
 }
