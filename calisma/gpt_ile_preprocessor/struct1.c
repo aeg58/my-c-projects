13a735ebb7645d9b537ceb1175ec68146887677e
Ali Erdem <alierdemgdk58@gmail.com>
2025-11-09 00:33:52 +0300

    auto(save): 2025-11-09 00:33:52

--- calisma/gpt_ile_preprocessor/struct1.c
+++ calisma/gpt_ile_preprocessor/struct1.c
@@ -14,8 +14,5 @@ int main()
     s1.age = 21;
     s1.grade = 90;
 
-    // buraya 3 satır: s1.name, s1.age, s1.grade değerlerini ata
-
-    // buraya 1 satır: printf ile ekrana bastır
     printf("Name : %s\n, Age : %d\n,Grade : %d\n",s1.name, s1.age, s1.grade);
 }
