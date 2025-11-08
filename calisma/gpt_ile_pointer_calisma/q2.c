20caee693ce9b4922c365046a64fbd13191a2db1
Ali Erdem <alierdemgdk58@gmail.com>
2025-11-08 15:12:20 +0300

    auto(save): 2025-11-08 15:12:20

--- calisma/gpt_ile_pointer_calisma/q2.c
+++ calisma/gpt_ile_pointer_calisma/q2.c
@@ -17,7 +17,7 @@ int main()
         {
             if (*(*(arr + 0) + 0) > max)
                 max = *(*(arr + 0) + 0);
-            if (*(*(arr+0)+0)<)    
+            if (*(*(arr+0)+0)<min)    
             }
     }
 }
