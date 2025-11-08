10aef9fbc4bfa2e0a1188cbba55af633fe8f81ac
Ali Erdem <alierdemgdk58@gmail.com>
2025-11-09 00:31:21 +0300

    auto(save): 2025-11-09 00:31:21

--- calisma/gpt_ile_preprocessor/struct1.c
+++ calisma/gpt_ile_preprocessor/struct1.c
@@ -9,7 +9,8 @@ struct Student {
 int main() {
     struct Student s1;
     s1.name = "Ali";
-    s1.age = 21
+    s1.age = 21;
+    s
 
     // buraya 3 satır: s1.name, s1.age, s1.grade değerlerini ata
 
