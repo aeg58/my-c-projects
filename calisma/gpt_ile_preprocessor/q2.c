91ed0d50fb071fea5a6cf14521fdfce73ff5efbf
Ali Erdem <alierdemgdk58@gmail.com>
2025-11-08 20:59:03 +0300

    auto(save): 2025-11-08 20:59:03

--- calisma/gpt_ile_preprocessor/q2.c
+++ calisma/gpt_ile_preprocessor/q2.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
-#include "functions.h"
-
-int add(int a, int b) {
-    return a + b;
-}
+#include "functions.h"   // tekrar include ediyoruz ama header guard sayesinde sorun yok
 
 int main() {
     printf("%d\n", add(5, 3));
+    return 0;
 }
