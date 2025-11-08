71a13344ab523629b55fdb3987e74141bdf5f640
Ali Erdem <alierdemgdk58@gmail.com>
2025-11-08 15:14:19 +0300

    auto(save): 2025-11-08 15:14:19

--- calisma/gpt_ile_pointer_calisma/q3.c
+++ calisma/gpt_ile_pointer_calisma/q3.c
@@ -6,13 +6,5 @@ int main()
         {4, 2, 9},
         {5, 7, 1}};
 
-        for (for (size_t i = 0; i < count; i++)
-        {
-            /* code */
-        }
-         i = 0; i < count; i++)
-        {
-            /* code */
-        }
-        
+        fpr
 }
