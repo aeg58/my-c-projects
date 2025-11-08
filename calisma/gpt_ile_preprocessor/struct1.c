438205d6263508117aa581b5ca1b7650aeebb043
Ali Erdem <alierdemgdk58@gmail.com>
2025-11-09 00:30:45 +0300

    auto(save): 2025-11-09 00:30:45

---
+++ calisma/gpt_ile_preprocessor/struct1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+struct Student {
+    char name[50];
+    int age;
+    float grade;
+};
+
+int main() {
+    struct Student s1;
+
+    // buraya 3 satır: s1.name, s1.age, s1.grade değerlerini ata
+
+    // buraya 1 satır: printf ile ekrana bastır
+}
