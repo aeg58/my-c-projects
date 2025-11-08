6029799cb2f44e6b19eee7e08e3801451b6428f1
Ali Erdem <alierdemgdk58@gmail.com>
2025-11-08 19:11:02 +0300

    auto(save): 2025-11-08 19:11:02

--- calisma/gpt_ile_preprocessor/q2.c
+++ calisma/gpt_ile_preprocessor/q2.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #define DEBUG
 
-int main(){
+int main()
+{
     int x = 5
 
-
-    #ifdef+
-}
+#ifdef DEBUG
+        printf("Debug mode\n");
+#else
+        printf("Normal mode\n");
+#endif
