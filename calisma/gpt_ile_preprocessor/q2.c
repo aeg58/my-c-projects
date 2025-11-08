d6d1356d0e40f5489018436fdb373f24a40d3c82
Ali Erdem <alierdemgdk58@gmail.com>
2025-11-08 19:22:31 +0300

    auto(save): 2025-11-08 19:22:31

--- calisma/gpt_ile_preprocessor/q2.c
+++ calisma/gpt_ile_preprocessor/q2.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #define VERSION 2
 
-#ifdef VERSION
+#ifdef __________
     printf("This is Version 2 of the program\n");
 #else
     printf("This is Version 1 of the program\n");
 #endif
+VERSION 2
